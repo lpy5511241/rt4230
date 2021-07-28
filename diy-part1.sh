2360a43c494d0e500c71abdfce769592dcd260b0
wzywzy74 <wzywzy74@163.com>
2021-07-28 09:43:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,6 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git openwrt-package https://github.com/Lienol/openwrt-package' >>feeds.conf.default
+
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
